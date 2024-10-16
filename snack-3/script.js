b25701bd519e78dd1449a9b96c81250d5bba72f4
salvoparisi <psalvo065@gmail.com>
2024-10-16 13:37:02 +0200

    add snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,10 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+let numberPlus=[]
+for(let i=0; i<numbers.length; i++){
+    numberPlus.push(numbers[i]+1)
+}
+console.log(numberPlus);
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
