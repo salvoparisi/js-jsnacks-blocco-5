6ee05be3be598db40ad4815745bf6f3f33013389
salvoparisi <psalvo065@gmail.com>
2024-10-16 13:47:30 +0200

    add snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,12 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+let even=[]
+nums.forEach(function(num){
+    if(num%2==0){
+        even.push(num)
+    }
+})
+console.log(even);
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
